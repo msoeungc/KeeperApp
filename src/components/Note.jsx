1b72b33b4a819b90686b0783f8fb0ede4fa72896
msoeungc <msoeungc@gmail.com>
2021-05-05 13:20:47 -0700

    Initial Commit, create footer, header, note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(props) {
+    return (
+        <div className="note">
+            <h1 className="h1">{props.title}</h1>
+            <p className="p">{props.content}</p>
+        </div>
+    )
+}
+
+export default Note;
